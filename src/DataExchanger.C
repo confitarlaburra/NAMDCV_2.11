7c0eac42ee159d39ed6a69dcd972459c36e6f9ef
confitarlaburra <jgarate@dlab.cl>
2016-12-16 17:01:03 -0300

    first

---
+++ src/DataExchanger.C
@@ -0,0 +1,169 @@
+/**
+***  Copyright (c) 1995, 1996, 1997, 1998, 1999, 2000 by
+***  The Board of Trustees of the University of Illinois.
+***  All rights reserved.
+**/
+#include <stdio.h>
+#include "DataExchanger.h"
+#include "ProcessorPrivate.h"
+#include "common.h"
+
+#if CMK_HAS_PARTITION
+#ifdef CmiMyPartitionSize
+extern "C" {
+  void setDefaultPartitionParams() {
+    // if(!CmiMyNodeGlobal()) printf("NAMD setDefaultPartitionParams called\n");
+    CmiSetPartitionScheme(3);  // recursive bisection
+  }
+}
+#endif
+#endif
+
+CpvDeclare(int, breakScheduler);
+
+//functions to receive and invoke chare's entry methods
+extern "C" {
+  void packSend(int dst, int dstPart, char *data, int size, int handler) {
+    int msgsize = sizeof(DataMessage) + size;
+    DataMessage *dmsg = (DataMessage *)CmiAlloc(msgsize);
+    dmsg->setMessage(data,CkMyPe(),CmiMyPartition(),size,handler);
+#if CMK_HAS_PARTITION
+    CmiInterSyncSendAndFree(dst,dstPart,msgsize,(char*)dmsg);
+#else
+    CmiSyncSendAndFree(dst,msgsize,(char*)dmsg);
+#endif
+  }
+
+  void recvData(DataMessage *dmsg) {
+    Pointer msg(dmsg);
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].recv_data(msg);
+  }
+
+  void recvAck(DataMessage *dmsg) {
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].recv_ack();
+    CmiFree(dmsg);
+  }
+
+  void recvBcast(DataMessage *dmsg) {
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].recv_bcast();
+    CmiFree(dmsg);
+  }
+
+  void recvRed(DataMessage *dmsg) {
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].recv_red();
+    CmiFree(dmsg);
+  }
+
+  void replica_send(char *sndbuf, int sendcount, int destPart, int destPE) {
+    Pointer sendPointer(sndbuf);
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].send(sendPointer,sendcount,destPart,destPE); 
+    CpvAccess(breakScheduler) = 0;
+    while(!CpvAccess(breakScheduler)) CsdSchedulePoll();
+  }
+
+  void replica_recv(DataMessage **precvMsg, int srcPart, int srcPE) {
+    Pointer recvPointer((char *) precvMsg);
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].recv(recvPointer,srcPart,srcPE);
+    CpvAccess(breakScheduler) = 0;
+    while(!CpvAccess(breakScheduler)) CsdSchedulePoll();
+  }
+
+  void replica_sendRecv(char *sndbuf, int sendcount, int destPart, int destPE, DataMessage **precvMsg, int srcPart, int srcPE)  {
+    Pointer sendPointer(sndbuf);
+    Pointer recvPointer((char *) precvMsg);
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].sendRecv(sendPointer,sendcount,destPart,destPE,recvPointer,srcPart,srcPE);
+    CpvAccess(breakScheduler) = 0;
+    while(!CpvAccess(breakScheduler)) CsdSchedulePoll();
+  }
+
+  void replica_barrier() {
+    CPROXY_DE(CkpvAccess(BOCclass_group).dataExchanger)[CkMyPe()].barrier();
+    CpvAccess(breakScheduler) = 0;
+    while(!CpvAccess(breakScheduler)) CsdSchedulePoll();
+  }
+
+  void replica_bcast(char *buf, int count, int root) {
+    if ( root != 0 ) NAMD_bug("replica_bcast root must be zero");
+    int rank = CmiMyPartition();
+    int size = CmiNumPartitions();
+    int i;
+    for ( i=1; i<size; i*=2 );
+    for ( i/=2; i>0; i/=2 ) {
+      if ( rank & (i - 1) ) continue;
+      if ( rank & i ) {
+        int src = rank - i;
+        //CkPrintf("rank %d recv from %d\n", rank, src);
+        DataMessage *recvMsg = NULL;
+        replica_recv(&recvMsg, src, CkMyPe());
+        if ( recvMsg == NULL ) NAMD_bug("recvMsg == NULL in replica_bcast");
+        if ( recvMsg->size != count ) NAMD_bug("size != count in replica_bcast");
+        memcpy(buf, recvMsg->data, count);
+        CmiFree(recvMsg);
+      } else {
+        int dst = rank + i;
+        if ( dst < size ) {
+          //CkPrintf("rank %d send to %d\n", rank, dst);
+          replica_send(buf, count, dst, CkMyPe());
+        }
+      }
+    }
+  }
+
+  void replica_min_double(double *dat, int count) {
+    int rank = CmiMyPartition();
+    int size = CmiNumPartitions();
+    for ( int i=1; i<size; i*=2 ) {
+      if ( rank & i ) {
+        int dst = rank - i;
+        //CkPrintf("rank %d send to %d\n", rank, dst);
+        replica_send((char*)dat, count * sizeof(double), dst, CkMyPe());
+      } else {
+        int src = rank + i;
+        if ( src < size ) {
+          //CkPrintf("rank %d recv from %d\n", rank, src);
+          DataMessage *recvMsg = NULL;
+          replica_recv(&recvMsg, src, CkMyPe());
+          if ( recvMsg == NULL ) NAMD_bug("recvMsg == NULL in replica_bcast");
+          if ( recvMsg->size != count * sizeof(double) ) NAMD_bug("size != count in replica_min_double");
+          double *rdat = new double[count];
+          memcpy(rdat, recvMsg->data, count * sizeof(double));
+          CmiFree(recvMsg);
+          for ( int j=0; j<count; ++j ) {
+            if ( rdat[j] < dat[j] ) dat[j] = rdat[j];
+          }
+          delete [] rdat;
+        }
+      }
+      if ( rank & (2 * i - 1) ) break;
+    }
+    replica_bcast((char*)dat, count * sizeof(double), 0);
+  }
+} //endof extern C
+
+//======================================================================
+// Public functions
+//----------------------------------------------------------------------
+DataExchanger::DataExchanger()
+{
+  CpvInitialize(int, breakScheduler);
+  CpvAccess(breakScheduler) = 1;
+  if(CmiMyPartition() == 0) 
+    parent = -1;
+  else 
+    parent = (CmiMyPartition()+1)/TREE_WIDTH - 1;
+  firstChild = (CmiMyPartition()+1)*TREE_WIDTH - 1;
+  numChildren = CmiNumPartitions() - firstChild;
+  if(numChildren > TREE_WIDTH)
+    numChildren = TREE_WIDTH;
+  recv_data_idx = CmiRegisterHandler((CmiHandler)recvData);
+  recv_ack_idx = CmiRegisterHandler((CmiHandler)recvAck);
+  recv_red_idx = CmiRegisterHandler((CmiHandler)recvRed);
+  recv_bcast_idx = CmiRegisterHandler((CmiHandler)recvBcast);
+}
+
+//----------------------------------------------------------------------
+DataExchanger::~DataExchanger(void)
+{ }
+
+
+#include "DataExchanger.def.h"
---
+++ src/colvarcomp_angles_BAK.C
@@ -0,0 +1,607 @@
+
+#include "colvarmodule.h"
+#include "colvar.h"
+#include "colvarcomp.h"
+
+#include <cmath>
+
+
+
+colvar::angle::angle (std::string const &conf)
+  : cvc (conf)
+{
+  function_type = "angle";
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  parse_group (conf, "group1", group1);
+  parse_group (conf, "group2", group2);
+  parse_group (conf, "group3", group3);
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+  if (get_keyval (conf, "oneSiteSystemForce", b_1site_force, false)) {
+    cvm::log ("Computing system force on group 1 only");
+  }
+  x.type (colvarvalue::type_scalar);
+}
+
+
+colvar::angle::angle (cvm::atom const &a1,
+                      cvm::atom const &a2,
+                      cvm::atom const &a3)
+  : group1 (std::vector<cvm::atom> (1, a1)),
+    group2 (std::vector<cvm::atom> (1, a2)),
+    group3 (std::vector<cvm::atom> (1, a3))
+{
+  function_type = "angle";
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  b_1site_force = false;
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+
+  x.type (colvarvalue::type_scalar);
+}
+
+
+colvar::angle::angle()
+{
+  function_type = "angle";
+  x.type (colvarvalue::type_scalar);
+}
+
+
+void colvar::angle::calc_value()
+{
+  group1.read_positions();
+  group2.read_positions();
+  group3.read_positions();
+
+  cvm::atom_pos const g1_pos = group1.center_of_mass();
+  cvm::atom_pos const g2_pos = group2.center_of_mass();
+  cvm::atom_pos const g3_pos = group3.center_of_mass();
+
+  r21  = cvm::position_distance (g2_pos, g1_pos);
+  r21l = r21.norm();
+  r23  = cvm::position_distance (g2_pos, g3_pos);
+  r23l = r23.norm();
+
+  cvm::real     const cos_theta = (r21*r23)/(r21l*r23l);
+
+  x.real_value = (180.0/PI) * std::acos (cos_theta);
+  std::cout<< "colvar_value "<<x.real_value <<std::endl;
+  std::cout<< "total_co2 "<<cos_theta * cos_theta <<std::endl;
+}
+
+
+void colvar::angle::calc_gradients()
+{
+  cvm::real const cos_theta = (r21*r23)/(r21l*r23l);
+  cvm::real const dxdcos = -1.0 / std::sqrt (1.0 - cos_theta*cos_theta);
+
+  dxdr1 = (180.0/PI) * dxdcos *
+    (1.0/r21l) * ( r23/r23l + (-1.0) * cos_theta * r21/r21l );
+
+  dxdr3 = (180.0/PI) * dxdcos *
+    (1.0/r23l) * ( r21/r21l + (-1.0) * cos_theta * r23/r23l );
+
+  for (size_t i = 0; i < group1.size(); i++) {
+    group1[i].grad = (group1[i].mass/group1.total_mass) *
+      (dxdr1);
+  }
+
+  for (size_t i = 0; i < group2.size(); i++) {
+    group2[i].grad = (group2[i].mass/group2.total_mass) *
+      (dxdr1 + dxdr3) * (-1.0);
+  }
+
+  for (size_t i = 0; i < group3.size(); i++) {
+    group3[i].grad = (group3[i].mass/group3.total_mass) *
+      (dxdr3);
+  }
+}
+
+void colvar::angle::calc_force_invgrads()
+{
+  // This uses a force measurement on groups 1 and 3 only
+  // to keep in line with the implicit variable change used to
+  // evaluate the Jacobian term (essentially polar coordinates
+  // centered on group2, which means group2 is kept fixed
+  // when propagating changes in the angle)
+
+  if (b_1site_force) {
+    group1.read_system_forces();
+    cvm::real norm_fact = 1.0 / dxdr1.norm2();
+    ft.real_value = norm_fact * dxdr1 * group1.system_force();
+  } else {
+    group1.read_system_forces();
+    group3.read_system_forces();
+    cvm::real norm_fact = 1.0 / (dxdr1.norm2() + dxdr3.norm2());
+    ft.real_value = norm_fact * ( dxdr1 * group1.system_force()
+                                + dxdr3 * group3.system_force());
+  }
+  return;
+}
+
+void colvar::angle::calc_Jacobian_derivative()
+{
+  // det(J) = (2 pi) r^2 * sin(theta)
+  // hence Jd = cot(theta)
+  const cvm::real theta = x.real_value * PI / 180.0;
+  jd = PI / 180.0 * (theta != 0.0 ? std::cos(theta) / std::sin(theta) : 0.0);
+}
+
+
+void colvar::angle::apply_force (colvarvalue const &force)
+{
+  if (!group1.noforce)
+    group1.apply_colvar_force (force.real_value);
+
+  if (!group2.noforce)
+    group2.apply_colvar_force (force.real_value);
+
+  if (!group3.noforce)
+    group3.apply_colvar_force (force.real_value);
+}
+
+
+colvar::dipole_angle::dipole_angle (std::string const &conf)
+: cvc (conf)
+  //: angle (conf)
+{
+  function_type = "dipole_angle";
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  parse_group (conf, "group1", group1);
+  parse_group (conf, "group2", group2);
+  parse_group (conf, "group3", group3);
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+  if (get_keyval (conf, "oneSiteSystemForce", b_1site_force, false)) {
+    cvm::log ("Computing system force on group 1 only");
+  }
+  x.type (colvarvalue::type_scalar);
+}
+
+
+colvar::dipole_angle::dipole_angle (cvm::atom const &a1,
+                      cvm::atom const &a2,
+                      cvm::atom const &a3)
+  : group1 (std::vector<cvm::atom> (1, a1)),
+    group2 (std::vector<cvm::atom> (1, a2)),
+    group3 (std::vector<cvm::atom> (1, a3))
+{
+  function_type = "dipole_angle";
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  b_1site_force = false;
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+
+  x.type (colvarvalue::type_scalar);
+}
+
+
+colvar::dipole_angle::dipole_angle()
+{
+  function_type = "dipole_angle";
+  x.type (colvarvalue::type_scalar);
+}
+
+#include <gnu/libc-version.h>
+#include "math.h"
+#include <fenv.h>
+void colvar::dipole_angle::calc_value()
+{
+  //feenableexcept(FE_INVALID | FE_OVERFLOW|FE_UNDERFLOW);
+  std::cout<< "calc_value"<<std::endl;
+  group1.read_positions();
+  group2.read_positions();
+  group3.read_positions();
+
+  cvm::atom_pos const g1_pos = group1.center_of_mass();
+  std::cout<< "atom_pos g1 "<<group1.center_of_mass()<<std::endl;
+  cvm::atom_pos const g2_pos = group2.center_of_mass();
+  cvm::atom_pos const g3_pos = group3.center_of_mass();
+  std::cout<< "atom_pos g2 "<<group2.center_of_mass()<<std::endl;
+  std::cout<< "atom_pos g3 "<<group3.center_of_mass()<<std::endl;
+
+  //r21 = group1.dipole(g1_pos);
+  r21 = cvm::position_distance(g2_pos, group1.dipole(g1_pos));
+  //r21 = cvm::position_distance(g2_pos, g1_pos);
+  std::cout<<"dipole module "<<sqrt (r21[0]*r21[0]+r21[1]*r21[1]+r21[2]*r21[2])<<std::endl;
+  r21l = r21.norm();
+  std::cout<<"dipole norm "<<r21l <<std::endl;
+  std::cout<<"dipole "<<r21 <<std::endl;
+  r23  = cvm::position_distance (g2_pos, g3_pos);
+  r23l = r23.norm();
+
+  cvm::real     const cos_theta = (r21*r23)/(r21l*r23l);
+
+  x.real_value = (180.0/PI) * std::acos (cos_theta);
+  std::cout<< "colvar_value "<<x.real_value <<std::endl;
+}
+
+
+void colvar::dipole_angle::calc_gradients()
+{
+  std::cout<< "calc_gradients"<<std::endl;
+  cvm::real const cos_theta = (r21*r23)/(r21l*r23l);
+  //en este caso x es la variable colectiva, y dxdcos es la derivada
+  //de la variable colectiva respecto a cos \theta
+  //b_1site_force = true;
+  std::cout<< "b_1site_force "<<b_1site_force <<std::endl;
+  cvm::real const dxdcos = -1.0 / std::sqrt (1.0 - cos_theta*cos_theta);
+
+  dxdr1 = (180/PI)*dxdcos; //implementacion nueva
+  //std::cout<< "180.0/PI "<<180.0/PI <<std::endl;
+  std::cout<< "cos_theta "<< cos_theta <<std::endl;
+
+  //dxdr1 = (180.0/PI) * dxdcos *
+  //(1.0/r21l) * ( r23/r23l + (-1.0) * cos_theta * r21/r21l );//comentar para implementacion nueva
+
+  double total_charge = 0.0;
+  for (size_t i = 0; i < group1.size(); i++) {
+    //std::cout<<"CHARGE "<<group1[i].charge<<std::endl;
+    total_charge += group1[i].charge;
+  }
+  
+  double total_force=0.0;
+  double total_cos2=0.0;
+  for (size_t i = 0; i < group1.size(); i++) {
+    /*group1[i].grad = ((group1[i].charge - ((group1[i].mass/group1.total_mass) *
+		total_charge) * (r23/r23l))/r21l - cos_theta*(r21/r21l)*(1.0/r21l)*(group1[i].charge -
+						     ((group1[i].mass/group1.total_mass)
+						     * total_charge) )) * (dxdr1);*/// primero
+
+    /*group1[i].grad = (1/r21l)*((group1[i].charge - ((group1[i].mass/group1.total_mass) *
+		total_charge) * (r23/r23l)) - cos_theta*(r21/r21l)*(group1[i].charge -
+						     ((group1[i].mass/group1.total_mass)
+						     * total_charge) )) * (dxdr1);*/// solo factorizado el modulo del dipolo, funciona igual a "primero"
+
+    group1[i].grad =(group1[i].charge - (group1[i].mass/group1.total_mass) *
+     total_charge)*  (1/r21l)* ((r23/r23l) - cos_theta*(r21/r21l)) * (dxdr1);// optimizado
+    
+    /*group1[i].grad =(group1[i].charge) *  (1/r21l)*( (r23/r23l) - cos_theta*(r21/r21l)) * (dxdr1);*///para el agua
+    
+    
+    std::cout<<"group1[i].mass/group1.total_mass * total_charge "<<group1[i].mass/group1.total_mass * total_charge<<std::endl;
+    std::cout<<"group1[i].mass/group1.total_mass "<<group1[i].mass/group1.total_mass<<std::endl;
+    std::cout<<"total_charge "<<total_charge<<std::endl;
+    std::cout<<"r23/r23l "<<r23/r23l <<std::endl;
+    std::cout<<"group1["<<i<<"]"<<group1[i].grad <<std::endl;
+
+    /*total_force +=  ((group1[i].charge - (group1[i].mass/group1.total_mass *
+					   total_charge) * (r23/r23l))/r21l -
+      cos_theta*(r21/r21l)*(1/r21l)*(group1[i].charge -(group1[i].mass/group1.total_mass *
+      total_charge) )) * (dxdr1);*/ 
+
+    total_force += (group1[i].charge - ((group1[i].mass/group1.total_mass) *
+					  total_charge)/r21l) * ((r23/r23l) - cos_theta  * (r21/r21l)) * (dxdr1);
+
+    //total_force_Ox = (180.0/PI)*dxdcos;
+    total_cos2 = cos_theta *cos_theta ;
+    
+    //(group1[i].mass/group1.total_mass);
+  }
+  std::cout<<"TOTAL_COS2 "<<total_cos2<<std::endl;
+  std::cout<<"TOTAL_FORCES "<<total_force<<std::endl;
+  /*
+  for (size_t i = 0; i < group1.size(); i++) {
+    group1[i].grad = (group1[i].mass/group1.total_mass) *
+      (dxdr1);
+    std::cout<<"TOTAL FORCE "<<group1[i].grad<<std::endl;
+    
+    // total_force += (group1[i].mass/group1.total_mass) *
+    //(dxdr1);
+    
+    }*/
+
+  //std::cout<<"TOTAL FORCE "<<total_force<<std::endl;
+  
+  /*for (size_t i = 0; i < group1.size(); i++) {
+    total_force += group1[i].grad;
+    std::cout<<"TOTAL FORCE "<<total_force<<std::endl;
+    }*/
+
+
+
+  /*for (size_t i = 0; i < group2.size(); i++) {
+    group2[i].grad = (group2[i].mass/group2.total_mass) *
+      (dxdr1 + dxdr3) * (-1.0);
+  }
+
+  for (size_t i = 0; i < group3.size(); i++) {
+    group3[i].grad = (group3[i].mass/group3.total_mass) *
+      (dxdr3);
+      }*/
+}
+
+void colvar::dipole_angle::calc_force_invgrads()
+{
+  std::cout<< "calc_force_invgrads"<<std::endl;
+  // This uses a force measurement on groups 1 and 3 only
+  // to keep in line with the implicit variable change used to
+  // evaluate the Jacobian term (essentially polar coordinates
+  // centered on group2, which means group2 is kept fixed
+  // when propagating changes in the angle)
+
+  if (b_1site_force) {
+    group1.read_system_forces();
+    cvm::real norm_fact = 1.0 / dxdr1.norm2();
+    ft.real_value = norm_fact * dxdr1 * group1.system_force();
+  } else {
+    group1.read_system_forces();
+    group3.read_system_forces();
+    cvm::real norm_fact = 1.0 / (dxdr1.norm2() + dxdr3.norm2());
+    ft.real_value = norm_fact * ( dxdr1 * group1.system_force()
+                                + dxdr3 * group3.system_force());
+  }
+  return;
+}
+
+void colvar::dipole_angle::calc_Jacobian_derivative()
+{
+  std::cout<< "calc_Jacobian_derivate"<<std::endl;
+  // det(J) = (2 pi) r^2 * sin(theta)
+  // hence Jd = cot(theta)
+  const cvm::real theta = x.real_value * PI / 180.0;
+  jd = PI / 180.0 * (theta != 0.0 ? std::cos(theta) / std::sin(theta) : 0.0);
+}
+
+
+void colvar::dipole_angle::apply_force (colvarvalue const &force)
+{
+  std::cout<<"apply_force"<<std::endl;
+  if (!group1.noforce){
+    group1.apply_colvar_force (force.real_value);
+    std::cout<<"force.real_value "<<force.real_value<<std::endl;
+    std::cout<<"SALIDA DE APPLY FORCE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"<<std::endl;
+    //std::cout<< "group1.noforce = "<<group1.noforce<<std::endl;
+  }
+  
+  /*if (!group2.noforce)
+    group2.apply_colvar_force (force.real_value);
+
+  
+  if (!group3.noforce)
+    group3.apply_colvar_force (force.real_value);
+  */
+}
+
+
+//---------------------------------------------------------------------------------
+
+
+colvar::dihedral::dihedral (std::string const &conf)
+  : cvc (conf)
+{
+  function_type = "dihedral";
+  period = 360.0;
+  b_periodic = true;
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  if (get_keyval (conf, "oneSiteSystemForce", b_1site_force, false)) {
+    cvm::log ("Computing system force on group 1 only");
+  }
+  parse_group (conf, "group1", group1);
+  parse_group (conf, "group2", group2);
+  parse_group (conf, "group3", group3);
+  parse_group (conf, "group4", group4);
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+  atom_groups.push_back (&group4);
+
+  x.type (colvarvalue::type_scalar);
+}
+
+
+colvar::dihedral::dihedral (cvm::atom const &a1,
+                            cvm::atom const &a2,
+                            cvm::atom const &a3,
+                            cvm::atom const &a4)
+  : group1 (std::vector<cvm::atom> (1, a1)),
+    group2 (std::vector<cvm::atom> (1, a2)),
+    group3 (std::vector<cvm::atom> (1, a3)),
+    group4 (std::vector<cvm::atom> (1, a4))
+{
+  if (cvm::debug())
+    cvm::log ("Initializing dihedral object from atom groups.\n");
+
+  function_type = "dihedral";
+  period = 360.0;
+  b_periodic = true;
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  b_1site_force = false;
+
+  atom_groups.push_back (&group1);
+  atom_groups.push_back (&group2);
+  atom_groups.push_back (&group3);
+  atom_groups.push_back (&group4);
+
+  x.type (colvarvalue::type_scalar);
+
+  if (cvm::debug())
+    cvm::log ("Done initializing dihedral object from atom groups.\n");
+}
+
+
+colvar::dihedral::dihedral()
+{
+  function_type = "dihedral";
+  period = 360.0;
+  b_periodic = true;
+  b_inverse_gradients = true;
+  b_Jacobian_derivative = true;
+  x.type (colvarvalue::type_scalar);
+}
+
+
+void colvar::dihedral::calc_value()
+{
+  group1.read_positions();
+  group2.read_positions();
+  group3.read_positions();
+  group4.read_positions();
+
+  cvm::atom_pos const g1_pos = group1.center_of_mass();
+  cvm::atom_pos const g2_pos = group2.center_of_mass();
+  cvm::atom_pos const g3_pos = group3.center_of_mass();
+  cvm::atom_pos const g4_pos = group4.center_of_mass();
+
+  // Usual sign convention: r12 = r2 - r1
+  r12 = cvm::position_distance (g1_pos, g2_pos);
+  r23 = cvm::position_distance (g2_pos, g3_pos);
+  r34 = cvm::position_distance (g3_pos, g4_pos);
+
+  cvm::rvector const n1 = cvm::rvector::outer (r12, r23);
+  cvm::rvector const n2 = cvm::rvector::outer (r23, r34);
+
+  cvm::real const cos_phi = n1 * n2;
+  cvm::real const sin_phi = n1 * r34 * r23.norm();
+
+  x.real_value = (180.0/PI) * std::atan2 (sin_phi, cos_phi);
+  this->wrap (x);
+}
+
+
+void colvar::dihedral::calc_gradients()
+{
+  cvm::rvector A = cvm::rvector::outer (r12, r23);
+  cvm::real   rA = A.norm();
+  cvm::rvector B = cvm::rvector::outer (r23, r34);
+  cvm::real   rB = B.norm();
+  cvm::rvector C = cvm::rvector::outer (r23, A);
+  cvm::real   rC = C.norm();
+
+  cvm::real const cos_phi = (A*B)/(rA*rB);
+  cvm::real const sin_phi = (C*B)/(rC*rB);
+
+  cvm::rvector f1, f2, f3;
+
+  rB = 1.0/rB;
+  B *= rB;
+
+  if (std::fabs (sin_phi) > 0.1) {
+    rA = 1.0/rA;
+    A *= rA;
+    cvm::rvector const dcosdA = rA*(cos_phi*A-B);
+    cvm::rvector const dcosdB = rB*(cos_phi*B-A);
+    rA = 1.0;
+
+    cvm::real const K = (1.0/sin_phi) * (180.0/PI);
+
+	f1 = K * cvm::rvector::outer (r23, dcosdA);
+	f3 = K * cvm::rvector::outer (dcosdB, r23);
+	f2 = K * (cvm::rvector::outer (dcosdA, r12)
+		   +  cvm::rvector::outer (r34, dcosdB));
+  }
+  else {
+    rC = 1.0/rC;
+    C *= rC;
+    cvm::rvector const dsindC = rC*(sin_phi*C-B);
+    cvm::rvector const dsindB = rB*(sin_phi*B-C);
+    rC = 1.0;
+
+    cvm::real    const K = (-1.0/cos_phi) * (180.0/PI);
+
+    f1.x = K*((r23.y*r23.y + r23.z*r23.z)*dsindC.x
+              - r23.x*r23.y*dsindC.y
+              - r23.x*r23.z*dsindC.z);
+    f1.y = K*((r23.z*r23.z + r23.x*r23.x)*dsindC.y
+              - r23.y*r23.z*dsindC.z
+              - r23.y*r23.x*dsindC.x);
+    f1.z = K*((r23.x*r23.x + r23.y*r23.y)*dsindC.z
+              - r23.z*r23.x*dsindC.x
+              - r23.z*r23.y*dsindC.y);
+
+    f3 = cvm::rvector::outer (dsindB, r23);
+    f3 *= K;
+
+    f2.x = K*(-(r23.y*r12.y + r23.z*r12.z)*dsindC.x
+              +(2.0*r23.x*r12.y - r12.x*r23.y)*dsindC.y
+              +(2.0*r23.x*r12.z - r12.x*r23.z)*dsindC.z
+              +dsindB.z*r34.y - dsindB.y*r34.z);
+    f2.y = K*(-(r23.z*r12.z + r23.x*r12.x)*dsindC.y
+              +(2.0*r23.y*r12.z - r12.y*r23.z)*dsindC.z
+              +(2.0*r23.y*r12.x - r12.y*r23.x)*dsindC.x
+              +dsindB.x*r34.z - dsindB.z*r34.x);
+    f2.z = K*(-(r23.x*r12.x + r23.y*r12.y)*dsindC.z
+              +(2.0*r23.z*r12.x - r12.z*r23.x)*dsindC.x
+              +(2.0*r23.z*r12.y - r12.z*r23.y)*dsindC.y
+              +dsindB.y*r34.x - dsindB.x*r34.y);
+  }
+
+  for (size_t i = 0; i < group1.size(); i++)
+    group1[i].grad = (group1[i].mass/group1.total_mass) * (-f1);
+
+  for (size_t i = 0; i < group2.size(); i++)
+    group2[i].grad = (group2[i].mass/group2.total_mass) * (-f2 + f1);
+
+  for (size_t i = 0; i < group3.size(); i++)
+	group3[i].grad = (group3[i].mass/group3.total_mass) * (-f3 + f2);
+
+  for (size_t i = 0; i < group4.size(); i++)
+    group4[i].grad = (group4[i].mass/group4.total_mass) * (f3);
+}
+
+
+void colvar::dihedral::calc_force_invgrads()
+{
+  cvm::rvector const u12 = r12.unit();
+  cvm::rvector const u23 = r23.unit();
+  cvm::rvector const u34 = r34.unit();
+
+  cvm::real const d12 = r12.norm();
+  cvm::real const d34 = r34.norm();
+
+  cvm::rvector const cross1 = (cvm::rvector::outer (u23, u12)).unit();
+  cvm::rvector const cross4 = (cvm::rvector::outer (u23, u34)).unit();
+
+  cvm::real const dot1 = u23 * u12;
+  cvm::real const dot4 = u23 * u34;
+
+  cvm::real const fact1 = d12 * std::sqrt (1.0 - dot1 * dot1);
+  cvm::real const fact4 = d34 * std::sqrt (1.0 - dot4 * dot4);
+
+  group1.read_system_forces();
+  if ( b_1site_force ) {
+    // This is only measuring the force on group 1
+    ft.real_value = PI/180.0 * fact1 * (cross1 * group1.system_force());
+  } else {
+    // Default case: use groups 1 and 4
+    group4.read_system_forces();
+    ft.real_value = PI/180.0 * 0.5 * (fact1 * (cross1 * group1.system_force())
+				      + fact4 * (cross4 * group4.system_force()));
+  }
+}
+
+
+void colvar::dihedral::calc_Jacobian_derivative()
+{
+  // With this choice of inverse gradient ("internal coordinates"), Jacobian correction is 0
+  jd = 0.0;
+}
+
+
+void colvar::dihedral::apply_force (colvarvalue const &force)
+{
+  if (!group1.noforce)
+    group1.apply_colvar_force (force.real_value);
+
+  if (!group2.noforce)
+    group2.apply_colvar_force (force.real_value);
+
+  if (!group3.noforce)
+    group3.apply_colvar_force (force.real_value);
+
+  if (!group4.noforce)
+    group4.apply_colvar_force (force.real_value);
+}
+
+
